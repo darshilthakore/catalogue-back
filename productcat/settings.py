5fbfaf08c713163d9285392c9c9bb03d23449ea7
Darshil Thakore <darshilthakore@gmail.com>
2020-08-10 00:55:35 +0530

    edited middleware in settings.py

--- productcat/settings.py
+++ productcat/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
